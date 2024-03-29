34ed541288efd4eea54a605733984fc95ecb7788
xu <3432607050@qq.com>
2024-03-29 16:23:19 +0800

    init

--- appfront/src/router/index.js
+++ appfront/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import shouye from '@/components/shouye'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'shouye',
+      component: shouye
     }
   ]
 })
